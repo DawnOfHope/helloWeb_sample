c29975330c6d7b31f253e8e7f6402851d9c47cac
DawnOfHope <DawnOfHope.zora@gmail.com>
2016-11-23 14:19:01 +0800

    增加使用jQuery去讀取json檔

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 "use strict";
 
 var express = require('express');
+var bootstrap = require("express-bootstrap-service");
 var app = express();
 var port = 8081;
 
 app.use(express.static(__dirname + '/app'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use(function(req,res){
         res.sendFile(__dirname  +   '/app/index.html')
         });
@@ -16,5 +20,3 @@ app.listen(port,function(error){
             console.info("Listening on port %s. Open up http://localhost:%s/",port, port)
            }
         })
-
-
